c9b91ecf1d7b388431b7765ed0d9314ec6138a7d
alsduq2813 <alsduq2813@GD18044422.Netmarble.Corp>
2018-09-18 15:59:09 +0900

    임시저장

--- first/src/main/java/first/common/controller/CommonController.java
+++ first/src/main/java/first/common/controller/CommonController.java
@@ -9,8 +9,12 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.log4j.Logger;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import first.common.common.CommandMap;
 import first.common.service.CommonService;
@@ -37,5 +41,20 @@ public class CommonController {
         response.getOutputStream().flush();
         response.getOutputStream().close();
     }
+    
+    @RequestMapping(value = "/chatting.do", method = RequestMethod.GET)
+	public ModelAndView chat(ModelAndView mv) {
+		mv.setViewName("chat/chattingview");
+		
+		//사용자 정보 출력(세션)//
+		User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		System.out.println("user name :" + user.getUsername());
+				
+		System.out.println("normal chat page");
+		
+		mv.addObject("userid", user.getUsername());
+		
+		return mv;
+	}
 
 }
